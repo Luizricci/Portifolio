e28df1491d7b125c747640c25b3c43d810e8a6e0
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-05-17 23:17:32 -0300

    Feat: add mani items

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,8 +1,29 @@
+import styles from './home.module.css';
+import Image from 'next/image';
+
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Portfólio</h1>
-      <p className="mt-4 text-lg">Em construção...</p>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h2 className={styles.name}>Luiz Henrique</h2>
+                <h3 className={styles.subTitle}>Desenvolvedor de Software</h3>
+            </div>
+            <div className={styles.profile}>
+                <div className={styles.imageContainer}>
+                    <Image src="/images/profile.svg" alt="Minha Foto" width={200} height={200} className={styles.image} />
+                </div>
+                <div className={styles.textContainer}>
+                    <h2 className={styles.sobre}>Sobre</h2>
+                    <p className={styles.description}>
+                        Olá, sou um desenvolvedor de software apaixonado por tecnologia e inovação estou sempre 
+                        em busca de novos desafios e oportunidades para aprender e crescer na minha carreira.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.habilidades}>
+                <h2 className={styles.habilidadesTitle}>Habilidades</h2>
+                
+            </div>
+        </div>
+    );
 }
