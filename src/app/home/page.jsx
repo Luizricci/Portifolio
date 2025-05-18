78f664c9059bcc43a67eebef3f01e00d19907702
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-05-17 21:46:14 -0300

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Portfólio</h1>
+      <p className="mt-4 text-lg">Em construção...</p>
+    </div>
+  );
+}
